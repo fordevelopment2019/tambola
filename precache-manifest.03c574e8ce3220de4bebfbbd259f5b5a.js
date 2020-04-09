2d331c0afcce79c8343a9e22ac8e2d5381e2ace6
Ankur Mittal <amittal1@kalido.local>
2020-04-09 23:32:59 +0530

    Updates

---
+++ precache-manifest.03c574e8ce3220de4bebfbbd259f5b5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1b66681fc0f157b3cf6f647c3b419c",
+    "url": "/tambola/index.html"
+  },
+  {
+    "revision": "e79fefa1c3b80b58735f",
+    "url": "/tambola/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b808416abd062512dca4",
+    "url": "/tambola/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e79fefa1c3b80b58735f",
+    "url": "/tambola/static/js/2.3d049ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tambola/static/js/2.3d049ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b808416abd062512dca4",
+    "url": "/tambola/static/js/main.79672815.chunk.js"
+  },
+  {
+    "revision": "7b2e6a74a3cf22b0d060",
+    "url": "/tambola/static/js/runtime-main.4fe26c42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tambola/static/media/logo.5d5d9eef.svg"
+  }
+]);
